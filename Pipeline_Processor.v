3c812516f244d5aa517a6173d21939c07113dea2
kiran_kumar_siripurapu <148669920+kiran7904@users.noreply.github.com>
2025-02-13 18:53:28 +0530

    Pipeline_Processor.v

--- Pipeline_Processor.v
+++ Pipeline_Processor.v
@@ -7,11 +7,20 @@ module Pipeline_Processor(
   reg [7:0]decode;
   reg [7:0]exe;
   reg [7:0]wb;
+  reg [7:0]regfile [3:0];
+  reg [7:0]memfile [3:0];
   always@(posedge clk)begin
     fetch<=instr;
     decode<=fetch;
-    exe<=decode;
+      case(decode[7:6])
+      2'b00:exe<=regfile[decode[5:4]]+regfile[decode[3:2]];
+      2'b01:exe<=regfile[decode[5:4]]-regfile[decode[3:2]];
+      2'b10:exe<=memfile[decode[3:0]];
+      default:exe<=8'b00000000;
+      endcase
     wb<=exe;
+    if(decode[7:6]!=2'b10)
+      regfile[decode[5:4]]<=wb;
     out<=wb;
   end
 endmodule
