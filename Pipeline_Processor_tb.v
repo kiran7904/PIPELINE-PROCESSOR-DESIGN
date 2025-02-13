17c56cfe5be0e76c349846bb80853c1918f4c507
kiran_kumar_siripurapu <148669920+kiran7904@users.noreply.github.com>
2025-02-13 18:53:59 +0530

    Pipeline_Processor_tb.v

--- Pipeline_Processor_tb.v
+++ Pipeline_Processor_tb.v
@@ -5,7 +5,12 @@ module Pipeline_Processor_tb;
   Pipeline_Processor pp(instr,clk,out);
   always #5 clk=~clk;
   initial begin
-    clk=0;instr=8'h00;
-    #10 instr=8'hAC;
+    clk=0;instr=8'b00001000;
+    #10 instr=8'b01010000;
+    #10 instr=8'b10010010;
+    #10 $stop;
+  end
+  initial begin
+  $monitor("time=%0t,instr=%b,out=%b",$time,instr,out);
   end
 endmodule
